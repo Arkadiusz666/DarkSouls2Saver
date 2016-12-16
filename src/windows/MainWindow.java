bd6d92525a149abd5a79a01b597c1be9aef85480
Arkadiusz666 <arkadiusz666@gmail.com>
2016-12-16 19:17:55 +0100

    minor refactoring

--- src/windows/MainWindow.java
+++ src/windows/MainWindow.java
@@ -13,9 +13,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-
-import static com.sun.glass.ui.Cursor.setVisible;
 
 /**
  * Created by Arkadiusz666 on 2016-11-15.
@@ -35,72 +32,46 @@ public class MainWindow extends JFrame {
         setContentPane(rootPanel);
 
         pack();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
         saveGameDateLabel.setText(FileOperator.getGameSaveDate());
         backupSaveGameDateLabel.setText(FileOperator.getBackupSaveDate());
         Color defaultBgColor = rootPanel.getBackground();
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                FileOperator.saveGame();
-                backupSaveGameDateLabel.setText(FileOperator.getBackupSaveDate());
+        saveButton.addActionListener(e -> {
+            FileOperator.saveGame();
+            backupSaveGameDateLabel.setText(FileOperator.getBackupSaveDate());
 
-                rootPanel.setBackground(defaultBgColor);
-            }
+            rootPanel.setBackground(defaultBgColor);
         });
-        loadButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                FileOperator.loadGame();
-                rootPanel.setBackground(defaultBgColor);
-            }
+        loadButton.addActionListener(e -> {
+            FileOperator.loadGame();
+            rootPanel.setBackground(defaultBgColor);
         });
 
         Timer SimpleTimer = new Timer(500, new ActionListener() {
             String lastDate = FileOperator.getGameSaveDate();
+
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 saveGameDateLabel.setText(FileOperator.getGameSaveDate());
                 if (!lastDate.equals(saveGameDateLabel.getText())) {
-                    lastDate=saveGameDateLabel.getText();
+                    lastDate = saveGameDateLabel.getText();
                     if (playSoundWhenGameIsSavedCheckBox.isSelected()) {
                         playSound();
                     }
                 }
-
                 if (!saveGameDateLabel.getText().equals(backupSaveGameDateLabel.getText())) {
                     rootPanel.setBackground(Color.red);
                 }
             }
         });
         SimpleTimer.start();
-
     }
-//todo
+
     private void playSound() {
         Toolkit.getDefaultToolkit().beep();
     }
-
-//    public static synchronized void playSound(final String url) {
-//        new Thread(new Runnable() {
-//            // The wrapper thread is unnecessary, unless it blocks on the
-//            // Clip finishing; see comments.
-//            public void run() {
-//                try {
-//                    Clip clip = AudioSystem.getClip();
-//                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-//                            Main.class.getResourceAsStream("" + url));
-//                    clip.open(inputStream);
-//                    clip.start();
-//                } catch (Exception e) {
-//                    System.err.println(e.getMessage());
-//                }
-//            }
-//        }).start();
-//    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
